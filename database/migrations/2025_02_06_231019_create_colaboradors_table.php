8db08300e8cd21130f2c38d45d91074a5b5f0294
Alisson-Ribeiro <alisson-ohkamy@hotmail.com>
2025-02-06 20:46:39 -0300

    :construction: colaboradors migration file set

--- database/migrations/2025_02_06_231019_create_colaboradors_table.php
+++ database/migrations/2025_02_06_231019_create_colaboradors_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('colaboradors', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('cpf', 14)->unique();
+            $table->foreignId('unidade_id')->constrained('unidades')->onDelete('cascade'); // FK para a tabela unidades
             $table->timestamps();
         });
     }
