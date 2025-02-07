1bdf2760663bab4d57ebe166874b54089d576d37
Alisson-Ribeiro <alisson-ohkamy@hotmail.com>
2025-02-06 21:07:35 -0300

    :construction: grupo_economicos migration file set

--- database/migrations/2025_02_06_230556_create_grupo_economicos_table.php
+++ database/migrations/2025_02_06_230556_create_grupo_economicos_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('grupo_economicos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
             $table->timestamps();
         });
     }
