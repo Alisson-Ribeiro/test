73846109656fcfb7a360ee8e7d8470aaf2e5f3a5
Alisson-Ribeiro <alisson-ohkamy@hotmail.com>
2025-02-07 21:10:17 -0300

    :construction: routes inserted at routes/web.php file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GrupoEconomicoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('grupo_economicos', GrupoEconomicoController::class);
