3deb62067cb87e77744015baa87048035ce784d0
Alisson-Ribeiro <alisson-ohkamy@hotmail.com>
2025-02-09 18:40:56 -0300

    :construction: Controller files created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UnidadeController;
+use App\Http\Controllers\BandeiraController;
+use App\Http\Controllers\ColaboradorController;
 use App\Http\Controllers\GrupoEconomicoController;
 
-Route::resource('grupo_economicos', GrupoEconomicoController::class);
+Route::resource('grupo_economico', GrupoEconomicoController::class);
+Route::resource('bandeira', BandeiraController::class);
+Route::resource('unidade', UnidadeController::class);
+Route::resource('colaborador', ColaboradorController::class);
